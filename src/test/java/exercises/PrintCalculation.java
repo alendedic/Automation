9b3f92b5340e10d2738c960bae6e7034fd0f5223
alendedic <alen.dedic@amplitudo.me>
2022-09-20 15:12:02 +0200

    calculationCommit

---
+++ src/test/java/exercises/PrintCalculation.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class PrintCalculation {
+    public static void main(String[] args) {
+        System.out.println("     299");
+        System.out.println("+    800");
+        System.out.println("--------");
+        System.out.println("    1099");
+    }
+}
