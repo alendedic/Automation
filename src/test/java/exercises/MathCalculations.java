d1d802454ce6c3b92ada0b9b4165d6b6d31ad61a
alendedic <alen.dedic@amplitudo.me>
2022-09-20 23:00:57 +0200

    MathCalculations Commit

---
+++ src/test/java/exercises/MathCalculations.java
@@ -0,0 +1,20 @@
+package exercises;
+
+public class MathCalculations {
+    public static void main(String[] args) {
+        double a = 2.8;
+        double b = 3.1;
+        double c = 6.0;
+        int x=2;
+        System.out.println(a+b);
+        System.out.println(Math.abs(a));
+        System.out.println(Math.round(a));
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.exp(a));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.pow(x,c));
+    }
+}
+
