6f23d28ecf91c75b5a1ef6f84eab1cfec4e73576
alendedic <alen.dedic@amplitudo.me>
2022-09-26 09:17:37 +0200

    Function Solver Commit

---
+++ src/test/java/exercises/FunctionSolver.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class FunctionSolver {
+    public static void main(String[] args) {
+        System.out.println("Insert the number:");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        if (0 <= x && x <= 100) {
+            System.out.println("f(x)= " + (2 * x));
+            System.out.println("g(x)= " + Math.pow(x,2));
+        } else {
+            System.out.println("f(x)=" + 0);
+            System.out.println("g(x)=" + 0);
+        }
+    }
+}
