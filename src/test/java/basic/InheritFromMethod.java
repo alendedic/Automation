9884df87cfcda620fb974cece2bbdf9fde3602b2
alendedic <alen.dedic@amplitudo.me>
2022-09-28 09:15:29 +0200

    bugfix delete line

--- src/test/java/basic/InheritFromMethod.java
+++ src/test/java/basic/InheritFromMethod.java
@@ -1,10 +1,11 @@
 package basic;
-
 public class InheritFromMethod extends Method{
     public static void main(String[] args) {
         calculate(5, 2);
+
         Method method = new Method();
         method.greetMe();
+
         calculate(20, 5, 3);
     }
 }
