6bbaf7b06b6bdcb143e54cd2639197051eaa063c
alendedic <alen.dedic@amplitudo.me>
2022-09-29 10:10:13 +0200

    bugfix delete empty lane and add lines bellow

--- src/test/java/basic/InheritFromMethod.java
+++ src/test/java/basic/InheritFromMethod.java
@@ -2,10 +2,8 @@ package basic;
 public class InheritFromMethod extends Method{
     public static void main(String[] args) {
         calculate(5, 2);
-
         Method method = new Method();
         method.greetMe();
-
         calculate(20, 5, 3);
     }
 }
